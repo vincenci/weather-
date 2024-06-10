1209cf43eb5a73a817558d7f22f54f04e7951f28
vincenci <vincyvine558@gmail.com>
2024-06-10 23:25:01 +0300

    updated footer

--- src/footer.js
+++ src/footer.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Footer(){
     return(
-          <footer>This project was coded by <a href="https://www.shecodes.io/" target="_blank"
-                            rel="noopener noreferrer">SheCodes</a> and is <a
-                            href="https://github.com/shecodesio/weather" target="_blank"
+          <footer>This project was coded by <a href="https://github.com/vincenci" target="_blank"
+                            rel="noopener noreferrer">vincencia</a> and is <a
+                            href="https://github.com/vincenci/weather-" target="_blank"
                             rel="noopener noreferrer">open-sourced on GitHub</a> and <a
-                            href="https://shecodes-weather.netlify.app/" target="_blank"
+                            href="https://shimmering-marzipan-d4b83f.netlify.app/" target="_blank"
                             rel="noopener noreferrer">hosted on Netlify</a></footer>
                 
     )
